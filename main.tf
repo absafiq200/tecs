1828814d42f674c09bef7cd3686ef3b8a282a6bb
shafeeq.bari <shafeeq.bari@dish.com>
2022-09-30 15:26:16 -0600

    initial

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ data "template_file" "container_tmpl" {
 }
 
 resource "aws_ecs_task_definition" "my_td" {
+  container_definitions    = data.template_file.container_tmpl.rendered
   family                   = "test_ts"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 512
   memory                   = "1024"
-  container_definitions    = data.template_file.container_tmpl.rendered
   task_role_arn            = aws_iam_role.ecs_role.arn
   execution_role_arn       = aws_iam_role.ecs_role.arn
 }
