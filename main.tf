d211d187f4757f2540bb0acf8dafd69082d57be3
shafeeq.bari <shafeeq.bari@dish.com>
2022-10-01 09:25:52 -0600

    initial

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 data "template_file" "container_tmpl" {
-  template = file("container_def.json.tpl")
+  template = file("container_def.json")
 
   vars = {
     "container_name" = "test-td",
     "imageurl" : "absafiq2000/demoapp:latest",
-    "containerport" : "8080"
+    "containerport" : 8080
   }
 }
 
