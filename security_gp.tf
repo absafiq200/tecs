da03996a773c9e71ed9a20fb574630b306a8bc4e
shafeeq.bari <shafeeq.bari@dish.com>
2022-10-03 11:01:39 -0600

    initial

--- security_gp.tf
+++ security_gp.tf
@@ -1,54 +1,54 @@
 //security group for alb
 resource "aws_security_group" "alb_sg" {
-    name = "my-alb-sg"
-    vpc_id = var.vpc_id
-    ingress {
-        from_port = var.from_port_ib
-        to_port = var.to_port_ib
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+  name   = "my-alb-sg"
+  vpc_id = var.vpc_id
+  ingress {
+    from_port   = var.from_port_ib
+    to_port     = var.to_port_ib
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags {
-        Name = "my-alb-sg"
-    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "my-alb-sg"
+  }
 }
 
 //Security group for ecs task
 resource "aws_security_group" "ecs_task_sg" {
-    name
-    vpc_id
+  name   = "ecs-task-sg"
+  vpc_id = var.vpc_id
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]        
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port = 0
-        to_port = 0
-        protocol= "-1"
-        security_groups = [aws_security_group.alb_sg]
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.alb_sg]
 
-    }
+  }
 
-    egress {
-        from_port = 0
-        to_port= 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags {
-        Name = "dev"
-    }
+  tags {
+    Name = "dev"
+  }
 
 }
